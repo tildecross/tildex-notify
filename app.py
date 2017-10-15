47c24634af4a8c68af3b0e42e6ff62516cb0220f
Clemens Wolff <clewolff@microsoft.com>
2017-10-14 22:48:33 -0400

    Extract logic into helper function

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import os
 import sys
 from pushbullet import Pushbullet
 
-if len(sys.argv) >= 3:
+
+def create_note(title, content):
     api_key = os.environ["PB_API_KEY"]
     pb = Pushbullet(api_key)
+    pb.push_note(title, content)
+
+
+if len(sys.argv) >= 3:
     title = sys.argv[1]
     body = sys.argv[2]
-    
-    pb.push_note(title, body)
+    create_note(title, body)
 else:
     print("Error: Missing arguments")
